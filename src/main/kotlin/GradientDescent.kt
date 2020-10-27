2cfd1a71de9031c2351d4253f7ab1beb41978d55
Odum <kristianodum@gmail.com>
2020-10-27 09:24:37 +0100

    Odums findings

--- src/main/kotlin/GradientDescent.kt
+++ src/main/kotlin/GradientDescent.kt
@@ -5,19 +5,181 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import org.ejml.simple.SimpleMatrix
 import java.awt.Color
-import javax.swing.plaf.ColorUIResource
+import java.lang.Exception
 import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.random.Random
 
-const val h = 0.00001 // small number..
+const val h = 0.0001 // small number..
 val gr = (sqrt(5.0) + 1.0) / 2.0
 const val tolerance = 0.000000000001
 var gss_b = 0.1
 
+fun rms(average: Double, new: Double, epsilon: Double = 0.001, gamma:Double = 0.9)
+        = sqrt((gamma * average + (1 - gamma) * new.pow(2.0)) + epsilon)
+
+fun gradientDescentNesterovLearningRate(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
+    val m = global_m.copy()
+    var best = Double.MAX_VALUE
+    val velocities = m.create { _ -> .0 }
+    val alpha = .0
+    val learningRate = .0001
+    var lastGradient = m.create { _ -> .0 }
+
+    repeat (epochs) {
+        val g_nag = gradient(m + velocities.scale(alpha), f)
+        val deltas = g_nag.create { _ -> 0.0 }
+
+        for (i in 0 until g_nag.numElements) {
+            deltas[i] = (lastGradient[i] - deltas[i])
+        }
+
+        val g = deltas.elementSum()
+
+        for (i in 0 until m.numElements) {
+            velocities[i] = alpha * velocities[i] + learningRate * g
+
+            m[i] += velocities[i]
+        }
+        lastGradient = g_nag
+
+        val c = cost(m, f)
+        if(c < best)
+            best = c
+        print("\r" + c)
+    }
+
+    println("\nBest: $best")
+    return m
+}
+
+fun gradientDescentDynamicLearningRate(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
+    val m = global_m.copy()
+    var best = Double.MAX_VALUE
+    var best_m = m.create { _ -> .0 }
+    val upscaleFactor = 1.2
+    val downscaleFactor = 0.5
+    val prevGradient = m.create { _ -> 0.0 }
+    val learningRates = m.create { _ -> 0.001 }
+    val minimaFound = m.create { _ -> -1.0}
+
+    repeat (epochs) {
+        val g = gradient(m, f)
+
+        for (i in 0 until m.numElements) {
+            if (g[i] > 0)
+                m[i] -= learningRates[i]
+            else
+                m[i] += learningRates[i]
+
+            if (prevGradient[i] * g[i] > 0)
+                learningRates[i] *= upscaleFactor
+            else {
+                learningRates[i] *= downscaleFactor
+                minimaFound[i] = 1.0
+            }
+
+            prevGradient[i] = g[i]
+        }
+        val c = cost(m, f)
+        if(c < best) {
+            best = c
+            best_m = m
+        }
+        print("\r" + c)
+    }
+
+    println("\nBest: $best")
+    return best_m
+}
+fun gradientDescentConstantLearningRate(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
+    val m = global_m.copy()
+    val stepSize = 0.0001
+    var best = Double.MAX_VALUE
+
+    repeat (epochs) {
+        val g = gradient(m, f)
+
+        for (i in 0 until m.numElements) {
+            if (g[i] > 0)
+                m[i] -= stepSize
+            else
+                m[i] += stepSize
+        }
+        val c = cost(m, f)
+        if(c < best)
+            best = c
+        print("\r" + c)
+    }
+
+    println("\nBest: $best")
+    return m
+}
+
+fun gradientDescentADADelta(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
+    val m = global_m.copy()
+    var best = Double.MAX_VALUE
+    val averageGradients = m.create { _ -> 0.0 }
+    val averageDeltas = m.create {_ -> 0.0}
+    val previousDeltas = m.create {_ -> 0.0}
+    val previousAverageDeltas = m.create { _ -> 0.0 }
+    val gamma = 0.9
+
+    repeat (epochs) {
+        val g = gradient(m, f)
+
+        for (i in 0 until m.numElements) {
+            // Calculate new delta
+            val delta = -(rms(previousAverageDeltas[i], previousDeltas[i]) / rms(averageDeltas[i], g[i]) * g[i])
+
+            averageGradients[i] = gamma * averageGradients[i] + (1 - gamma) * g[i].pow(2.0)
+
+            m[i] += delta
+            // calculate average delta
+            // Update previous deltas
+            previousAverageDeltas[i] = averageDeltas[i]
+            previousDeltas[i] = delta
+            averageDeltas[i] = gamma * averageDeltas[i] + (1 - gamma) * delta.pow(2.0)
+        }
+        val c = cost(m, f)
+        if(c < best)
+            best = c
+        print("\r" + c)
+    }
+    println("\nBest: $best")
+    return m
+}
+
+fun gradientDescentRMSProp(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
+    val m = global_m.copy()
+    val averageGradients  = m.create { _ -> .0 }
+    var best = Double.MAX_VALUE
+
+    val learningRate = 0.001
+    val eps = 1e-8
+
+    repeat (epochs) {
+        val g = gradient(m, f)
+
+        for (i in 0 until m.numElements) {
+            averageGradients[i] += rms(averageGradients[i], g[i])
+            val delta = -(learningRate / sqrt(averageGradients[i] + eps)) * g[i]
+
+            m[i] += delta
+        }
+        val c = cost(m, f)
+        if(c < best)
+            best = c
+        print("\r" + c)
+    }
+    println("\nBest: $best")
+
+    return m
+}
+
 fun gradientDescent(f: (SimpleMatrix) -> SimpleMatrix, cost: (SimpleMatrix, (SimpleMatrix) -> SimpleMatrix) -> Double, n: Int, nhat: Int, epochs: Int): SimpleMatrix {
-    var m = randMatrix(nhat, n, 0.0, 1.0)
+    var m = global_m.copy()
 
     println("m: $m")
     for (i in 0 until epochs) {
@@ -116,7 +278,7 @@ fun gradient(m: SimpleMatrix, f: (SimpleMatrix) -> SimpleMatrix): SimpleMatrix {
                 mutex.lock()
                 i++
                 gradient[elem] = g
-                print("\r${i.toDouble() / m.numElements}")
+                //print("\r${i.toDouble() / m.numElements}")
                 mutex.unlock()
             }
         }
