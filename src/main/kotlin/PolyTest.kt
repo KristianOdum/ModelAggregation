a5ed4d31b939e1f6ce790f197dd8df96883f2b43
Ragusaen <rtollu18@student.aau.dk>
2020-12-03 11:11:18 +0100

    Fix naming

--- src/main/kotlin/PolyTest.kt
+++ src/main/kotlin/PolyTest.kt
@@ -6,28 +6,28 @@ import gradientDescent.MMomentumGD
 import utility.*
 
 fun main() {
-    val model = T9ModelCreator().createModel(1)
+    val mc = T6ModelCreator()
+    val model = mc.createModel(1)
     val iterations = 100
     val tolerance = 0.05
     val derTolerance = 0.5
 
-    // Set this to your T number
-    val i = 9
+    val i = mc::class.java.name.takeWhile { it != 'M' }
 
     val costCalculator = CostCalculator(MonteCarloMeanCalculator(model.lumpingMatrix.numCols(), tolerance), model.function)
     val derCalculator = DerivativeCalculator(MonteCarloMeanCalculator(model.lumpingMatrix.numCols(), derTolerance), model.function)
 
-    val pso = CostFunctionOptimizerTester(3000, iterations, 300, "PSO_T$i.txt") {
+    val pso = CostFunctionOptimizerTester(3000, iterations, 300, "PSO_$i.txt") {
         val modelinfo = ModelInfo(randMatrix(model.lumpingMatrix.numRows(), model.lumpingMatrix.numCols(), -1.0 until 1.0), model.function)
         GeneralPSO(modelinfo, 1, 20, PSOInfo(3000), costCalculator)
     }
 
-    val line_search = CostFunctionOptimizerTester(1000, iterations, 10, "line_search_T$i.txt") {
+    val line_search = CostFunctionOptimizerTester(1000, iterations, 10, "line_search_$i.txt") {
         val modelinfo = ModelInfo(randMatrix(model.lumpingMatrix.numRows(), model.lumpingMatrix.numCols(), -1.0 until 1.0), model.function)
         GoldenSectionGD(modelinfo, derCalculator, costCalculator)
     }
 
-    val mmomentum = CostFunctionOptimizerTester(5000, iterations, 50, "mmomentum_T$i.txt") {
+    val mmomentum = CostFunctionOptimizerTester(5000, iterations, 50, "mmomentum_$i.txt") {
         val modelinfo = ModelInfo(randMatrix(model.lumpingMatrix.numRows(), model.lumpingMatrix.numCols(), -1.0 until 1.0), model.function)
         MMomentumGD(modelinfo, 0.003, 5000, derCalculator, costCalculator)
     }
