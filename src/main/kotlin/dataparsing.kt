6dfbf7f04c8abef1fc55055303ad0557f73d4977
Slorup <nicklas.slorup@yahoo.dk>
2020-12-06 13:14:13 +0100

    Big models

--- src/main/kotlin/dataparsing.kt
+++ src/main/kotlin/dataparsing.kt
@@ -5,7 +5,7 @@ import kotlin.math.pow
 data class IterationData(val epoch: Int, val cost: Double, val time: Int, val startCost: Double)
 
 fun main() {
-    val data = File("mmomentum_T14.txt").readText().split('\n').filter { it.isNotBlank() }.map {
+    val data = File("mmomentum_T7 (2).txt").readText().split('\n').filter { it.isNotBlank() }.map {
         val v = it.split(' ')
         IterationData(v[0].toInt(), v[1].toDouble(), v[2].toInt(), v[3].toDouble())
     }
